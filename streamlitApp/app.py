103383faef720989a3110da50d55463638631ba2
anapstl <ana_apostol@hotmail.com>
2025-07-14 23:47:51 +0200

    vuelvo a activar el menu de deploy

--- streamlitApp/app.py
+++ streamlitApp/app.py
@@ -20,9 +20,7 @@ st.markdown("""
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
+
             
 body {
     background-color: transparent;
